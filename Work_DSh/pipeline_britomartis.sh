3fbdb96f6b5bbff965f942002871982c70ae3835
DaSh-bash <70020998+DaSh-bash@users.noreply.github.com>
2023-03-10 14:43:52 +0100

    daily log update

---
+++ Work_DSh/pipeline_britomartis.sh
@@ -0,0 +1,50 @@
+Assmann:
+
+ssh -AX <username>@rackham.uppmax.uu.se
+
+ml bioinfo-tools dds-cli
+
+dds data ls --project "ngisthlm00193"
+     ︵
+ ︵ (  )   ︵
+(  ) ) (  (  )   SciLifeLab Data Delivery System
+ ︶  (  ) ) (    https://delivery.scilifelab.se/
+      ︶ (  )    CLI Version 2.2.62
+          ︶
+INFO     No saved token found, or token has expired, proceeding with authentication
+DDS username: dariash
+DDS password:
+╭─ Error ──────────────────────────────────────────────────────────────────────────────────────────╮
+│ Failed to authenticate user: Missing or incorrect credentials                                    │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────╯
+(base) [daria@rackham3 ~]$ dds data ls --project "ngisthlm00193"
+     ︵
+ ︵ (  )   ︵
+(  ) ) (  (  )   SciLifeLab Data Delivery System
+ ︶  (  ) ) (    https://delivery.scilifelab.se/
+      ︶ (  )    CLI Version 2.2.62
+          ︶
+INFO     No saved token found, or token has expired, proceeding with authentication
+DDS username: dariash
+DDS password:
+INFO     Please enter the one-time authentication code sent to your email address (leave empty to exit):
+Authentication one-time code: 11775319
+INFO     Listing files for project 'ngisthlm00193'
+
+ Files / directories in project: ngisthlm00193
+ └── P27562/
+
+
+
+screen -S britomartis
+
+ml bioinfo-tools dds-cli
+cd /proj/uppstore2017185/b2014034/private/raw_data/Assmann
+dds data get --get-all --project "ngisthlm00193"
+
+#screen -S britomartis_arxiv
+#ml bioinfo-tools dds-cli
+#cd /proj/uppoff2020002/private/raw_data_backups/Assmann
+#dds data get --get-all --project "ngisthlm00193"
+
+screen -r britomartis
