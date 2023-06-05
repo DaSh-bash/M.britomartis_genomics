a509291ea2124e3a704e5fab8ce865a8146924c0
DaSh-bash <70020998+DaSh-bash@users.noreply.github.com>
2023-06-05 09:37:34 +0200

    update

---
+++ Work_DSh/main.snakefile
@@ -0,0 +1,306 @@
+
+# The following rules will not be executed as jobs
+localrules: all
+
+rule all:
+    input:
+
+        "vep/74Females.SNPs.HF.mac2.txt",
+        "vep/74Females.SNPs.HF.mac1.txt"
+
+SAMPLES = ["AfGWo", "EthWo", "Dhole", "BlBJa", "SiSJa"]
+FILTER = ["mac2", "mac1"]
+
+# The following rules will not be executed as jobs
+localrules: all, makeGVCFList
+
+rule all:
+    input:
+        expand("outgroup/{prefix}.100S95F14R.chr1-38.{prefix2}.extract.vcf", prefix=SAMPLES, prefix2=FILTER),
+        expand("outgroup/{prefix}.74Females.chrX.{prefix2}.extract.vcf", prefix=SAMPLES, prefix2=FILTER)
+
+rule bwa:
+    input:
+        ref="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        r1="fastq/{prefix}_1.fastq.gz",
+        r2="fastq/{prefix}_2.fastq.gz"
+    output:
+        "bam/{prefix}.bam"
+    log:
+        "logs/bwa/{prefix}.log"
+    params:
+        rg=r"@RG\tID:{prefix}\tSM:{prefix}\tLB:{prefix}\tPL:Illumina"
+    threads: 20
+    shell:
+        "(bwa mem -R '{params.rg}' -t {threads} -M {input.ref} {input.r1} {input.r2} \
+         |samtools sort -m 6G -@{threads} -T $SNIC_TMP/{wildcards.prefix} - >{output}.tmp) 2>{log} \
+         && mv {output}.tmp {output}"
+
+rule index_bam:
+    input:
+        "bam/{prefix}.bam"
+    output:
+        "bam/{prefix}.bam.bai"
+    log:
+        "logs/samtools/index.{prefix}.log"
+    threads: 1
+    shell:
+        "samtools index {input} {output}"
+
+rule mkdupl:
+    input:
+        "bam/{prefix}.bam"
+    output:
+        bam="bam/{prefix}.md.bam",
+        metrics="bam/{prefix}.metrics"
+    log:
+        "logs/picard/{prefix}.mkdupl.log"
+    params:
+        picard="/sw/apps/bioinfo/picard/2.23.4/rackham/picard.jar",
+        mem="60g"
+    threads: 10
+    shell:
+        "(java -Xmx{params.mem} -jar {params.picard} MarkDuplicates INPUT={input} \
+         METRICS_FILE={output.metrics} TMP_DIR=$SNIC_TMP ASSUME_SORTED=true \
+         VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=TRUE OUTPUT={output.bam}.tmp) 2>{log} \
+         && mv {output.bam}.tmp {output.bam} && mv {output.bam}.tmp.bai {output.bam}.bai"
+
+rule haplotype_caller:
+    input:
+        bam="bam/{prefix}.md.bam",
+        ref="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa"
+    output:
+        "gvcf/{prefix}.g.vcf.gz"
+    log:
+        "logs/GATK/HaplotypeCaller.{prefix}.log"
+    params:
+        gatk="/sw/apps/bioinfo/GATK/3.8-0/GenomeAnalysisTK.jar",
+        mem="120g"
+    threads: 20
+    shell:
+        "(java -Xmx{params.mem} -Djava.io.tmpdir=$SNIC_TMP -jar {params.gatk} \
+        -T HaplotypeCaller -R {input.ref} -I {input.bam} --emitRefConfidence GVCF \
+        --variant_index_type LINEAR --variant_index_parameter 128000 -nct {threads} \
+        -jdk_deflater -jdk_inflater -o gvcf/{wildcards.prefix}.g.vcf.tmp.gz) 2>{log} \
+        && mv gvcf/{wildcards.prefix}.g.vcf.tmp.gz {output} \
+        && mv gvcf/{wildcards.prefix}.g.vcf.tmp.gz.tbi {output}.tbi"
+
+rule makeGVCFList:
+    input:
+        "gvcf/{prefix}.g.vcf.gz"
+    output:
+        "{prefix}.gvcf.list"
+    log:
+        "logs/misc/create.GVCFList.{prefix}.log"
+    shell:
+        "echo {input} >{output}"
+
+rule AllsitesGenotypeGVCFs:
+    input:
+        fa="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        list="{prefix}.gvcf.list"
+    output:
+        "vcf/{prefix}.vcf"
+    params:
+        gatk="/sw/apps/bioinfo/GATK/3.8-0/GenomeAnalysisTK.jar",
+        mem="120g"
+    log:
+        "logs/GATK/GenotypeGVCFs.{prefix}.log"
+    threads: 20
+    shell:
+        "java -Xmx{params.mem} -Djava.io.tmpdir=$SNIC_TMP -jar {params.gatk} \
+        -T GenotypeGVCFs -R {input.fa} --variant {input.list} -nt {threads} \
+        -allSites -o {output}.tmp && mv {output}.tmp {output}"
+
+rule zip:
+    input:
+        "vcf/{prefix}.vcf"
+    output:
+        "vcf/{prefix}.vcf.gz"
+    log:
+        "logs/zip/{prefix}.log"
+    threads: 1
+    shell:
+        "bgzip {input}"
+
+rule tabix:
+    input:
+        "vcf/{prefix}.vcf.gz"
+    output:
+        "vcf/{prefix}.vcf.gz.tbi"
+    params:
+        "-p vcf"
+    log:
+        "logs/tabix/{prefix}.log"
+    threads: 1
+    shell:
+        "tabix {params} {input}"
+
+rule extractBedSites:
+    input:
+        vcf="vcf/{species, \w+}.vcf.gz",
+        index="vcf/{species, \w+}.vcf.gz.tbi",
+        bed="bed/{filter}.bed"
+    output:
+        "outgroup/{species, \w+}.{filter}.extract.vcf"
+    log:
+        "logs/vcf/{species, \w+}.{filter}.extract.log"
+    threads: 1
+    shell:
+        "zcat {input.vcf} |grep -v '<NON_REF>' | \
+        intersectBed -header -a - -b {input.bed} >{output}"
+
+
+
+
+rule GenotypeGVCFs:
+    input:
+        fa="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        list="{prefix}.gvcf.list"
+    output:
+        "vcf/{prefix}.vcf"
+    params:
+        gatk="/sw/apps/bioinfo/GATK/3.8-0/GenomeAnalysisTK.jar",
+        mem="1000g"
+    log:
+        "logs/GATK/GenotypeGVCFs.{prefix}.log"
+    threads: 20
+    shell:
+        "java -Xmx{params.mem} -Djava.io.tmpdir=$SNIC_TMP -jar {params.gatk} \
+        -T GenotypeGVCFs -R {input.fa} --variant {input.list} -nt {threads} -o {output}.tmp && mv {output}.tmp {output}"
+
+rule zip:
+    input:
+        "vcf/{prefix}.vcf"
+    output:
+        "vcf/{prefix}.vcf.gz"
+    log:
+        "logs/zip/{prefix}.log"
+    threads: 1
+    shell:
+        "bgzip {input}"
+
+rule tabix:
+    input:
+        "vcf/{prefix}.vcf.gz"
+    output:
+        "vcf/{prefix}.vcf.gz.tbi"
+    params:
+        "-p vcf"
+    log:
+        "logs/tabix/{prefix}.log"
+    threads: 1
+    shell:
+        "tabix {params} {input}"
+
+rule idepth:
+    input:
+        index="vcf/{prefix}.vcf.gz.tbi",
+        vcf="vcf/{prefix}.vcf.gz"
+    output:
+        "coverage/{prefix}.vcf.idepth"
+    log:
+        "logs/coverage/{prefix}.log"
+    threads: 1
+    shell:
+        "vcftools --gzvcf {input.vcf} --stdout --depth >{output}"
+
+rule extractSNPs:
+    input:
+        fa="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        index="vcf/{prefix}.vcf.gz.tbi",
+        vcf="vcf/{prefix}.vcf.gz"
+    output:
+        "vcf/{prefix}.SNPs.vcf"
+    log:
+        "logs/GATK/{prefix}.extractSNPs.log"
+    params:
+        gatk="/sw/apps/bioinfo/GATK/3.8-0/GenomeAnalysisTK.jar",
+        mem="60g"
+    threads: 10
+    shell:
+        "java -Xmx{params.mem} -jar {params.gatk} -T SelectVariants \
+        -R {input.fa} -V {input.vcf} -selectType SNP -o {output}"
+
+
+rule filterSNPs:
+    input:
+        fa="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        index="vcf/{prefix}.SNPs.vcf.gz.tbi",
+        vcf="vcf/{prefix}.SNPs.vcf.gz"
+    output:
+        "vcf/{prefix}.SNPs.HF.vcf"
+    log:
+        "logs/GATK/{prefix}.HardFiltSNPs.log"
+    params:
+        gatk="/sw/apps/bioinfo/GATK/3.8-0/GenomeAnalysisTK.jar",
+        mem="60g",
+        filt=r"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"
+    threads: 10
+    shell:
+        "java -Xmx{params.mem} -jar {params.gatk} -T VariantFiltration \
+        -R {input.fa} -V {input.vcf} --filterExpression '{params.filt}' \
+        --filterName 'hard_filt' -o {output}"
+
+
+rule vcfFilter:
+    input:
+        index="vcf/{prefix}.vcf.gz.tbi",
+        vcf="vcf/{prefix}.vcf.gz",
+        dep="coverage/{prefix}.vcf.idepth"
+    output:
+        "vcf/{prefix}.mac2.vcf"
+    log:
+        "logs/filter/{prefix}.mac2.log"
+    params:
+        min=10,
+        minal=2,
+        maxal=2,
+        mac=2,
+        maxmiss=0.9,
+        GQ=30,
+    threads: 1
+    shell:
+        "m=`awk '(NR>1){{n++; sum+=$3}}END{{mean=sum/n; twice=2*mean; print twice}}' {input.dep}` && "
+        "vcftools --gzvcf {input.vcf} --out vcf/{wildcards.prefix} --mac {params.mac}  \
+        --max-alleles {params.maxal}  --min-alleles {params.minal}  --min-meanDP {params.min} \
+        --max-meanDP $m  --minGQ {params.GQ} --max-missing {params.maxmiss} --remove-filtered-all \
+        --recode --recode-INFO-all && mv vcf/{wildcards.prefix}.recode.vcf {output}"
+
+rule keepSingletonFilter:
+    input:
+        index="vcf/{prefix}.vcf.gz.tbi",
+        vcf="vcf/{prefix}.vcf.gz",
+        dep="coverage/{prefix}.vcf.idepth"
+    output:
+        "vcf/{prefix}.mac1.vcf"
+    log:
+        "logs/filter/{prefix}.mac1.log"
+    params:
+        min=10,
+        minal=2,
+        maxal=2,
+        maxmiss=0.9,
+        GQ=30,
+    threads: 1
+    shell:
+        "m=`awk '(NR>1){{n++; sum+=$3}}END{{mean=sum/n; twice=2*mean; print twice}}' {input.dep}` && "
+        "vcftools --gzvcf {input.vcf} --out vcf/{wildcards.prefix}  \
+        --max-alleles {params.maxal}  --min-alleles {params.minal}  --min-meanDP {params.min} \
+        --max-meanDP $m  --minGQ {params.GQ} --max-missing {params.maxmiss} --remove-filtered-all \
+        --recode --recode-INFO-all && mv vcf/{wildcards.prefix}.recode.vcf {output}"
+
+rule vep:
+    input:
+        vcf="vcf/{prefix}.vcf.gz",
+        index="vcf/{prefix}.vcf.gz.tbi",
+    output:
+        "vep/{prefix}.txt"
+    log:
+        "logs/vep/{prefix}.log"
+    params:
+        sp="canis_familiaris"
+    threads: 4
+    shell:
+        "vep --cache --dir $VEP_CACHE -i {input.vcf} -o {output} \
+        --species {params.sp} --fork {threads} --force_overwrite --sift b"
--- Work_DSh/pipeline_britomartis.sh
+++ Work_DSh/pipeline_britomartis.sh
@@ -179,3 +179,384 @@ awk -F, 'BEGIN{ORS=" ";} {print "[\""$8"_"$4"\"]=\""$1"\""}' M.britomartis_sampl
 ### Test sarek run - succesful
 
 interactive -A naiss2023-5-52 -p node -t 12:00:00
+
+
+#### Plan for further analysis
+<<todo:
+1. Try new trimming on 2 files from the test run within snakemake
+2. QC, decide how to trim the rest
+3. Assemble mtDNA for all the samples (also snakemake?)
+
+todo
+
+
+module load bioinfo-tools snakemake
+
+#Test data:
+wget https://github.com/snakemake/snakemake-tutorial-data/archive/v5.4.5.tar.gz
+tar --wildcards -xf v5.4.5.tar.gz --strip 1 "*/data"
+
+mkdir workflow
+touch workflow/Snakefile
+
+
+mkdir snakemake-tutorial
+curl -L https://api.github.com/repos/snakemake/snakemake-tutorial-data/tarball -o snakemake-tutorial-data.tar.gz
+tar --wildcards -xf snakemake-tutorial-data.tar.gz --strip 1 "*/data" "*/environment.yaml"
+mamba env create --name snakemake-tutorial --file environment.yaml
+conda activate snakemake-tutorial
+snakemake -np mapped_reads/A.bam
+snakemake --cores 1 mapped_reads/A.bam
+nano Snakefile
+snakemake --cores 1 mapped_reads/{A,B}.bam
+
+.
+├── data
+│   ├── genome.fa
+│   ├── genome.fa.amb
+│   ├── genome.fa.ann
+│   ├── genome.fa.bwt
+│   ├── genome.fa.fai
+│   ├── genome.fa.pac
+│   ├── genome.fa.sa
+│   └── samples
+│       ├── A.fastq
+│       ├── B.fastq
+│       └── C.fastq
+├── environment.yaml
+├── mapped_reads
+│   ├── A.bam
+│   └── B.bam
+├── Snakefile
+└── snakemake-tutorial-data.tar.gz
+
+#Writing rules!
+
+rule bwa:
+    input:
+        ref="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        r1="fastq/{prefix}_1.fastq.gz",
+        r2="fastq/{prefix}_2.fastq.gz"
+    output:
+        "bam/{prefix}.bam"
+    log:
+        "logs/bwa/{prefix}.log"
+    params:
+        rg=r"@RG\tID:{prefix}\tSM:{prefix}\tLB:{prefix}\tPL:Illumina"
+    threads: 20
+    shell:
+        "(bwa mem -R '{params.rg}' -t {threads} -M {input.ref} {input.r1} {input.r2} \
+         |samtools sort -m 6G -@{threads} -T $SNIC_TMP/{wildcards.prefix} - >{output}.tmp) 2>{log} \
+         && mv {output}.tmp {output}"
+
+
+         FASTP	fastp	0.23.2
+         FASTQC	fastqc	0.11.9
+
+
+fastp --in1 MUZAV26_94-L001_1.fastq.gz --in2 MUZAV26_94-L001_2.fastq.gz --out1 MUZAV26_94-L001_1.fastp.fastq.gz --out2 MUZAV26_94-L001_2.fastp.fastq.gz --json MUZAV26_94-L001.fastp.json --html MUZAV26_94-L001.fastp.html --thread 12 --detect_adapter_for_pe --disable_adapter_trimming --split_by_lines 200000000
+
+printf "%s\n" P27562_1039_S39_L001_R1_001.fastq.gz P27562_1039_S39_L001_R2_001.fastq.gz | while read f; do [[ $f =~ ^BAJK_1_2016-L001.* ]] || ln -s $f BAJK_1_2016-L001_$f ; done
+    fastqc --quiet --threads 4 BAJK_1_2016-L001*
+
+    cat <<-END_VERSIONS > versions.yml
+    "NFCORE_SAREK:SAREK:FASTQC":
+        fastqc: $( fastqc --version | sed -e "s/FastQC v//g" )
+    END_VERSIONS
+
+  [ ! -f  BAJK_1_2016-L001_1.fastq.gz ] && ln -sf P27562_1039_S39_L001_R1_001.fastq.gz BAJK_1_2016-L001_1.fastq.gz
+            [ ! -f  BAJK_1_2016-L001_2.fastq.gz ] && ln -sf P27562_1039_S39_L001_R2_001.fastq.gz BAJK_1_2016-L001_2.fastq.gz
+            fastp \
+                --in1 BAJK_1_2016-L001_1.fastq.gz \
+                --in2 BAJK_1_2016-L001_2.fastq.gz \
+                --out1 BAJK_1_2016-L001_1.fastp.fastq.gz \
+                --out2 BAJK_1_2016-L001_2.fastp.fastq.gz \
+                --json BAJK_1_2016-L001.fastp.json \
+                --html BAJK_1_2016-L001.fastp.html \
+                 \
+                 \
+                 \
+                --thread 12 \
+                --detect_adapter_for_pe \
+                --disable_adapter_trimming      --split_by_lines 200000000 \
+                2> BAJK_1_2016-L001.fastp.log
+
+            cat <<-END_VERSIONS > versions.yml
+            "NFCORE_SAREK:SAREK:FASTP":
+                fastp: $(fastp --version 2>&1 | sed -e "s/fastp //g")
+            END_VERSIONS
+
+
+
+rule bwa:
+    input:
+        ref="reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa",
+        r1="fastq/{prefix}_1.fastq.gz",
+        r2="fastq/{prefix}_2.fastq.gz"
+    output:
+        "bam/{prefix}.bam"
+    log:
+        "logs/bwa/{prefix}.log"
+    params:
+        rg=r"@RG\tID:{prefix}\tSM:{prefix}\tLB:{prefix}\tPL:Illumina"
+    threads: 20
+    shell:
+        "(bwa mem -R '{params.rg}' -t {threads} -M {input.ref} {input.r1} {input.r2} \
+         |samtools sort -m 6G -@{threads} -T $SNIC_TMP/{wildcards.prefix} - >{output}.tmp) 2>{log} \
+         && mv {output}.tmp {output}"
+
+
+
+
+
+
+module load fastp
+
+###Finding file pathes
+find /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x -name "*.fastq.gz"  | sort > LR999924.g.vcf.list
+
+fastp \
+    --in1 SMAL_2_1967-L001_1.fastp.fastq.gz \
+    --in2 SMAL_2_1967-L001_2.fastp.fastq.gz \
+    --out1 SMAL_2_1967-L001_1.fastpX.fastq.gz \
+    --out2 SMAL_2_1967-L001_2.fastpX.fastq.gz \
+    --json SMAL_2_1967-L001.fastpX.json \
+    --html SMAL_2_1967-L001.fastpX.html \
+    --thread 12 \
+    --detect_adapter_for_pe \
+    --split_by_lines 200000000
+
+Some chuncks:
+
+
+    /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/work/c7/ac12632666d326dcae9679d1a5ea38/0001.MUZAV26_94-L001_1.fastp.fastq.gz
+    /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/work/c7/ac12632666d326dcae9679d1a5ea38/0001.MUZAV26_94-L001_2.fastp.fastq.gz
+
+mv /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/work/c7/ac12632666d326dcae9679d1a5ea38/0001.MUZAV26_94-L001_1.fastp.fastq.gz SMAL_2_1967-L001_1.fastp.fastq.gz
+mv /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/work/c7/ac12632666d326dcae9679d1a5ea38/0001.MUZAV26_94-L001_2.fastp.fastq.gz SMAL_2_1967-L001_2.fastp.fastq.gz
+
+
+fastp \
+    --in1 SMAL_2_1967-L001_1.fastq.gz \
+    --in2 SMAL_2_1967-L001_2.fastq.gz \
+    --out1 SMAL_2_1967-L001_1.fastp.fastq.gz \
+    --out2 SMAL_2_1967-L001_2.fastp.fastq.gz \
+    --json SMAL_2_1967-L001.fastp.json \
+    --html SMAL_2_1967-L001.fastp.html \
+    --thread 12 \
+    --reads_to_process 1000 \
+    --detect_adapter_for_pe \
+
+
+
+ln -sf /proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1055/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1055_S55_L001_R1_001.fastq.gz SMAL_2_1967-L001_1.fastq.gz
+ln -sf /proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1055/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1055_S55_L001_R2_001.fastq.gz SMAL_2_1967-L001_2.fastq.gz
+
+/proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1055/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1055_S55_L001_R1_001.fastq.gz,/proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1055/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1055_S55_L001_R2_001.fastq.gz
+
+
+ln -sf /proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1015/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1015_S15_L001_R1_001.fastq.gz GAST_5_1943-L001_1.fastq.gz
+ln -sf /proj/uppstore2017185/b2014034/private/raw_data/Assmann/DataDelivery_2023-03-07_15-07-05_ngisthlm00193/files/P27562/P27562_1015/02-FASTQ/230303_A00689_0768_AHKFY7DSX5/P27562_1015_S15_L001_R2_001.fastq.gz GAST_5_1943-L001_2.fastq.gz
+
+fastp \
+    --in1 GAST_5_1943-L001_1.fastq.gz \
+    --in2 GAST_5_1943-L001_2.fastq.gz \
+    --out1 GAST_5_1943-L001_1.fastp.fastq.gz \
+    --out2 GAST_5_1943-L001_2.fastp.fastq.gz \
+    --json GAST_5_1943-L001.fastp.json \
+    --html GAST_5_1943-L001.fastp.html \
+    --thread 12 \
+    --reads_to_process 1000 \
+    --detect_adapter_for_pe \
+
+
+    fastp \
+        --in1 GAST_5_1943-L001_1.fastq.gz \
+        --in2 GAST_5_1943-L001_2.fastq.gz \
+        --out1 GAST_5_1943-L001_1.fastp.fastq.gz \
+        --out2 GAST_5_1943-L001_2.fastp.fastq.gz \
+        --json GAST_5_1943-L001.fastp.json \
+        --html GAST_5_1943-L001.fastp.html \
+        --thread 12 \
+        --reads_to_process 1000 \
+        --trim_front1=20 \
+        --trim_tail1=20 \
+        --detect_adapter_for_pe \
+        -p \
+        --adapter_fasta adapters.fasta
+
+>Illumina TruSeq Adapter Read 1
+AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG
+>Illumina TruSeq Adapter Read 2
+AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT
+>polyA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+>polyG
+
+fastp \
+    --in1 GAST_5_1943-L001_1.fastq.gz \
+    --in2 GAST_5_1943-L001_2.fastq.gz \
+    --out1 GAST_5_1943-L001_1.fastp.fastq.gz \
+    --out2 GAST_5_1943-L001_2.fastp.fastq.gz \
+    --json GAST_5_1943-L001.fastp.json \
+    --html GAST_5_1943-L001.fastp.html \
+    --thread 12 \
+    --reads_to_process 1000 \
+    --trim_front1=20 \
+    --trim_tail1=20 \
+    --detect_adapter_for_pe \
+    -p \
+    --adapter_fasta adapters.fasta
+
+
+    fastp \
+        --in1 GAST_5_1943-L001_1.fastq.gz \
+        --in2 GAST_5_1943-L001_2.fastq.gz \
+        --out1 GAST_5_1943-L001_1.fastp.fastq.gz \
+        --out2 GAST_5_1943-L001_2.fastp.fastq.gz \
+        --json GAST_5_1943-L001.fastp.json \
+        --html GAST_5_1943-L001.fastp.html \
+        --thread 12 \
+        --reads_to_process 1000 \
+        --trim_front1=20 \
+        --trim_tail1=20 \
+        --detect_adapter_for_pe \
+        -p \
+        -r \
+        --adapter_fasta adapters.fasta
+
+
+#### mt genome assembly
+
+module load GetOrganelle/1.7.7.0
+module load GetOrganelleDB
+
+ get_organelle_from_reads.py -1 Arabidopsis_simulated.1.fq.gz -2 Arabidopsis_simulated.2.fq.gz -t 1 -o Arabidopsis_simulated.plastome -F embplant_pt -R 10
+
+get_organelle_from_reads.py -1 /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_1.fastp.fastq.gz -2 /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_2.fastp.fastq.gz -t 1 -o SMAL_2_1967.mt -F animal_mt -R 10
+
+-1 ../00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_1.fastp.fastq.gz -2 ../00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_2.fastp.fastq.gz
+
+
+#SBATCH -A naiss2023-5-52
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 04:00:00
+#SBATCH -J mtgenome_assembly
+#SBATCH --mail-user=daria.shipilina@gmail.com
+
+module load GetOrganelle/1.7.7.0
+module load GetOrganelleDB
+
+get_organelle_from_reads.py -1 /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_1.fastp.fastq.gz -2 /crex/proj/uppstore2017185/b2014034_nobackup/Dasha/M.britomartis_Conservation/00_Mapping_Calling_sarek/00_Benchmarking4x/SMAL_2_1967-L001_2.fastp.fastq.gz -t 4 -o SMAL_2_1967.mt -F animal_mt -R 10
+
+
+
+
+###### Snakemake for organelle
+### working in separate file
+
+mamba env create --name mtgenome --file mapping.yaml
+conda activate snakemake-tutorial
+snakemake -np mapped_reads/A.bam
+snakemake --cores 1 mapped_reads/A.bam
+nano Snakefile
+snakemake --cores 1 mapped_reads/{A,B}.bam
+
+conda env update --name snakemake-tutorial --file environment.yaml
+
+
+#### Trying snakemake for mapping and calling
+
+#Added fastp/0.23.2
+conda env update --name snakemake-tutorial --file environment.yaml
+
+
+rule fastqc:
+    input:
+        "GAST_2_1941-L001_1.fastp.fastq.gz",
+        "GAST_2_1941-L001_2.fastp.fastq.gz"
+    output:
+        html_report = "GAST_2_1941_output.html",
+        zip_archive = "GAST_2_1941_output.zip"
+    shell:
+        """
+        fastqc {input} --threads 4 --outdir . &&
+        mv *fastqc.html {output.html_report} &&
+        mv *fastqc.zip {output.zip_archive}
+        """
+
+
+rule fastp_preprocess:
+    input:
+	      r1 = "GAST_5_1943-L001_1.fastq.gz",
+        r2 = "GAST_5_1943-L001_2.fastq.gz"
+    output:
+	      r1_out = "GAST_5_1943-L001_1.fastp.fastq.gz",
+        r2_out = "GAST_5_1943-L001_2.fastp.fastq.gz",
+        json_out = "GAST_5_1943-L001.fastp.json",
+        html_out = "GAST_5_1943-L001.fastp.html"
+    params:
+	      thread = 12,
+        reads_to_process = 1000
+    shell:
+	"""
+	fastp \
+        --in1 {input.r1} \
+        --in2 {input.r2} \
+        --out1 {output.r1_out} \
+        --out2 {output.r2_out} \
+        --json {output.json_out} \
+        --html {output.html_out} \
+        --thread {params.thread} \
+        --reads_to_process {params.reads_to_process} \
+        --detect_adapter_for_pe
+        """
+
+
+r1 = expand("../00_Mapping_Calling_sarek/00_Benchmarking4x/{sample}-L001_1.fastp.fastq.gz", sample=samples),
+r2 = expand("../00_Mapping_Calling_sarek/00_Benchmarking4x/{sample}-L001_2.fastp.fastq.gz", sample=samples
+
+
+rule fastqc:
+    input:
+        "data/samples/GAST_5_1943-L001_1.fastq.gz",
+        "data/samples/GAST_5_1943-L001_2.fastq.gz"
+    output:
+        html_report = "GAST_5_1943_output.html",
+        zip_archive = "GAST_5_1943_output.zip"
+    shell:
+        """
+        fastqc {input} --threads 4 --outdir . &&
+        mv *fastqc.html {output.html_report} &&
+        mv *fastqc.zip {output.zip_archive}
+        """
+
+rule fastp_preprocess:
+    input:
+	r1 = "data/samples/GAST_5_1943-L001_1.fastq.gz",
+        r2 = "data/samples/GAST_5_1943-L001_2.fastq.gz"
+    output:
+	r1_out = "GAST_5_1943-L001_1.fastp.fastq.gz",
+        r2_out = "GAST_5_1943-L001_2.fastp.fastq.gz",
+        json_out = "GAST_5_1943-L001.fastp.json",
+        html_out = "GAST_5_1943-L001.fastp.html"
+    params:
+	thread = 12,
+        reads_to_process = 1000
+    shell:
+	"""
+	fastp \
+        --in1 {input.r1} \
+        --in2 {input.r2} \
+        --out1 {output.r1_out} \
+        --out2 {output.r2_out} \
+        --json {output.json_out} \
+        --html {output.html_out} \
+        --thread {params.thread} \
+        --reads_to_process {params.reads_to_process} \
+        --detect_adapter_for_pe
+        """
+
+
+snakemake -s fastp.snakemake --verbose --debug-dag --use-conda
